4e101e61017023c2dfc1050900200224ad806142
Dan Pletzke <dpletzke@gmail.com>
2020-09-23 19:02:03 +0000

    handle user input working

--- play.js
+++ play.js
@@ -8,3 +8,34 @@ const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  
+  stdin.on('data', handleUserInput);  
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    console.log('Thanks for using me, ciao!');
+    process.exit();
+  }
+  if (data === 'b') {
+    console.log('beep!');
+  }
+}
+setupInput();
+
+
+
+
